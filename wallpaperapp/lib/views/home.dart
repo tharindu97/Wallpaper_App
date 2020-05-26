63e60ed701f0a0fb058d6646470191d13c71570b
tharindu97 <tharindukavishna@gmail.com>
2020-05-26 18:44:56 +0530

    step_01

---
+++ wallpaperapp/lib/views/home.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:wallpaperapp/widgets/widget.dart';
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: brandName(),
+        elevation: 0.0,
+      ),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            Container(
+              decoration: BoxDecoration(
+                color: Color(0xfff5f8fd),
+                borderRadius: BorderRadius.circular(30)
+              ),
+              padding: EdgeInsets.symmetric(horizontal: 24),
+              margin: EdgeInsets.symmetric(horizontal: 24),
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: TextField(
+                      decoration: InputDecoration(
+                        hintText: 'search wallpaper',
+                        border: InputBorder.none
+                      ),
+                    ),
+                  ),
+                  Icon(Icons.search)
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
