dd747d9f2ff3d48fa925dd2d3fb2ed30e787a7aa
tharindu97 <tharindukavishna@gmail.com>
2020-05-28 15:52:07 +0530

    1_35_07

--- wallpaperapp/lib/views/home.dart
+++ wallpaperapp/lib/views/home.dart
@@ -1,7 +1,13 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:wallpaperapp/data/data.dart';
 import 'package:wallpaperapp/model/categorie_model.dart';
+import 'package:wallpaperapp/model/wallpaper_model.dart';
 import 'package:wallpaperapp/widgets/widget.dart';
+import 'package:http/http.dart' as http;
+
+
 class Home extends StatefulWidget {
   @override
   _HomeState createState() => _HomeState();
@@ -10,13 +16,31 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
 
   List<CategoriesModel> categories = new List();
+  List<WallpaperModel> wallpapers = new List();
 
-  getTrendingwallpapers(){
-    
+  getTrendingwallpapers() async {
+    var response = await http.get("https://api.pexels.com/v1/curated",
+      headers: {
+        "Authorization" : apiKey
+      },
+    );
+    //print(response.body.toString());
+    Map<String, dynamic> jsonData = jsonDecode(response.body);
+    jsonData["photos"].forEach((element){
+      //print(element);
+      WallpaperModel wallpaperModel = new WallpaperModel();
+      wallpaperModel = WallpaperModel.fromMap(element);
+      wallpapers.add(wallpaperModel);
+    });
+
+    setState(() {
+      
+    }); 
   }
-
+ 
   @override
   void initState() {
+    getTrendingwallpapers();
     categories = getCategories();
     super.initState();
   }
@@ -30,47 +54,50 @@ class _HomeState extends State<Home> {
         title: brandName(),
         elevation: 0.0,
       ),
-      body: Container(
-        child: Column(
-          children: <Widget>[
-            Container(
-              decoration: BoxDecoration(
-                color: Color(0xfff5f8fd),
-                borderRadius: BorderRadius.circular(30)
-              ),
-              padding: EdgeInsets.symmetric(horizontal: 24),
-              margin: EdgeInsets.symmetric(horizontal: 24),
-              child: Row(
-                children: <Widget>[
-                  Expanded(
-                    child: TextField(
-                      decoration: InputDecoration(
-                        hintText: 'search wallpaper',
-                        border: InputBorder.none
+      body: SingleChildScrollView(
+        child: Container(
+          child: Column(
+            children: <Widget>[
+              Container(
+                decoration: BoxDecoration(
+                  color: Color(0xfff5f8fd),
+                  borderRadius: BorderRadius.circular(30)
+                ),
+                padding: EdgeInsets.symmetric(horizontal: 24),
+                margin: EdgeInsets.symmetric(horizontal: 24),
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: TextField(
+                        decoration: InputDecoration(
+                          hintText: 'search wallpaper',
+                          border: InputBorder.none
+                        ),
                       ),
                     ),
-                  ),
-                  Icon(Icons.search)
-                ],
+                    Icon(Icons.search)
+                  ],
+                ),
               ),
-            ),
-            SizedBox(height: 16,),
-            Container(
-              height: 80,
-              child: ListView.builder(
-                padding: EdgeInsets.symmetric(horizontal: 24),
-                itemCount: categories.length,
-                shrinkWrap: true,
-                scrollDirection: Axis.horizontal,
-                itemBuilder: (context, index){
-                  return CategoriesTitle(
-                    title: categories[index].categorieName,
-                    imgUrl: categories[index].imgUrl,
-                  );
-                }
+              SizedBox(height: 16,),
+              Container(
+                height: 80,
+                child: ListView.builder(
+                  padding: EdgeInsets.symmetric(horizontal: 24),
+                  itemCount: categories.length,
+                  shrinkWrap: true,
+                  scrollDirection: Axis.horizontal,
+                  itemBuilder: (context, index){
+                    return CategoriesTitle(
+                      title: categories[index].categorieName,
+                      imgUrl: categories[index].imgUrl,
+                    );
+                  }
+                ),
               ),
-            ),
-          ],
+              wallpapersList(wallpapers: wallpapers, context: context)
+            ],
+          ),
         ),
       ),
     );
@@ -97,7 +124,10 @@ class CategoriesTitle extends StatelessWidget {
             ),
           ),
           Container(
-            color: Colors.black26,
+            decoration: BoxDecoration(
+              color: Colors.black26,
+              borderRadius: BorderRadius.circular(8)
+            ),
             height: 50, width: 100,
             alignment: Alignment.center,
             child: Text(title, style: TextStyle(color: Colors.white, fontWeight: FontWeight.w500, fontSize: 16),),
--- wallpaperapp/lib/widgets/widget.dart
+++ wallpaperapp/lib/widgets/widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:wallpaperapp/model/wallpaper_model.dart';
 
 Widget brandName(){
   return Row(
@@ -7,5 +8,26 @@ Widget brandName(){
       Text('wallpaper',style: TextStyle(color: Colors.black87),),
       Text('Hub',style: TextStyle(color: Colors.blue))
     ],
+  );
+}
+
+Widget wallpapersList({List<WallpaperModel> wallpapers, context}){
+  return Container(
+    padding: EdgeInsets.symmetric(horizontal: 16),
+    child: GridView.count(
+      shrinkWrap: true,
+      physics: ClampingScrollPhysics(),
+      crossAxisCount: 2,
+      childAspectRatio: 0.6,
+      mainAxisSpacing: 6.0,
+      crossAxisSpacing: 6.0,
+      children: wallpapers.map((wallpapers){
+        return GridTile(
+          child: Container(
+            child: Image.network(wallpapers.src.portrait,fit: BoxFit.cover,),
+          )
+        );
+      }).toList(),
+    ),
   );
 }
